c8dd941f4907945cd5500d0c169472a9aa4f754a
pierre-lamarche <lamarche.p@gmail.com>
2017-01-29 13:14:54 +0100

    Correct the script for Filiben's method

--- src/quantile.R
+++ src/quantile.R
@@ -1,17 +1,17 @@
-quantile <- function(x, type = 1:10, probs = 0.5) {
+quantile <- function(x, type = 1:11, probs = 0.5) {
   # Compute the quantile for weighted data (e.g. survey data). Includes the 9 methods
-  # described by Hyndman and Fan (1996) + the one by Cunnane (1978).
+  # described by Hyndman and Fan (1996) + the one by Cunnane (1978) + the one by Filiben (1975).
   #
   # Args:
   #  x: numeric vector giving the values taken by the variable of interest.
-  #  type: an integer between 1 and 10 selecting one of the nine quantile algorithms detailed
-  #in Hyndman and Fan (1996), alternatively the one inspired from Cunnane (1978), as in the 
-  #Python scipy library.
+  #  type: an integer between 1 and 11 selecting one of the nine quantile algorithms detailed
+  #in Hyndman and Fan (1996), alternatively the one inspired from Cunnane (1978) or the one
+  #in Filiben (1975), as in the Python scipy library. See the references for more details.
   #  probs: numeric vector of probabilities with values between 0 and 1.
   #
   # Returns:
   #  The vector of quantile values.
-  if (!type %in% 1:10) stop("Parameter type must be an integer between 1 and 10.")
+  if (!type %in% 1:11) stop("Parameter type must be an integer between 1 and 11.")
   
   if (type %in% 1:9) {
     stats::quantile(x = x, type = type, probs = probs)
