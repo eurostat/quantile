6c8fc7bd72fb01d6a8eb0cb0fa90d26b8d840138
Jacopo Grazzini <gjacopo@gmail.com>
2017-03-06 01:35:37 +0100

    added for automatic loading

---
+++ src/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python -io_quantile
+# -*- coding: utf-8 -*-
+"""
+.. __init__
+
+**Description**
+
+Software-agnostic (re)implementations (R/SAS/Python/C) of common quantile estimation algorithms
+
+**About**
+
+This code is intended as a proof of concept for the following publication:
+* Grazzini J. and Lamarche P. (2017): Production of social statistics... goes social!, 
+    in Proc. New Techniques and Technologies for Statistics.
+
+Copyright (c) 2017, J.Grazzini & P.Lamarche, European Commission
+Licensed under [European Union Public License](https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11)
+"""
+
+__all__ = ['quantile', 'io_quantile' ]
+
